f8ee736e9773f6e64b03e0332b7f243921e5173a
gustavoameno <gustavoameno@gmail.com>
2020-09-14 17:16:16 -0300

    criando o repositorio

---
+++ Curso em Video/OlaMudo.py
@@ -0,0 +1,11 @@
+print("Ola, mundo!!!")
+
+print("Hello, world!!!")
+
+variavel = int(input("Entre com o número: "))
+if variavel >=4:
+    print("Variavel invalida")
+else:
+    print("Variavel Valida")
+print(variavel)
+print('Final de programa')
